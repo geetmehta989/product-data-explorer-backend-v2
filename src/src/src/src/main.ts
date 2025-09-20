6e1c3a2785bd3246e3e05f0e2388944b9f4804c9
geetmehta989 <103765083+geetmehta989@users.noreply.github.com>
2025-09-20 13:16:15 +0530

    main.ts

---
+++ src/src/src/src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Product Data Explorer API')
+    .setDescription('Backend API for Product Data Explorer')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
+}
+bootstrap();
